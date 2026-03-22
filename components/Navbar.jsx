28daa8995e1933e4ca72c3bad2fd1ddec12fb471
Kriti Goomer <kritigoomer@gmail.com>
2026-03-21 19:51:22 -0500

    change file structure and update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,16 @@
 const Navbar = () => {
 
     const navLinks = [
-        {name: "experience", link: "#"},
-        {name: "projects", link: "#"},
+        // {name: "experience", link: "#"},
+        // {name: "projects", link: "#"},
     ]
 
     return (
         <nav className="fixed flex align-center justify-center items-center hover:scale-[1.1] transition-transform">
-            <h1 className="font-heading text-4xl hover:text-amber-300 hover:scale-[1.1] transition-colors">kg</h1>
-            <div className="font-body flex gap-6 text-xl">
+            <a href="#">
+                <h1 className="font-heading text-4xl hover:text-amber-300 hover:scale-[1.1] transition-colors">kg</h1>
+            </a>
+            {/* <div className="font-body flex gap-6 text-xl">
                 {navLinks.map((link) => (
                     <a key = {link.name}
                     href={link.link} className="transition-transform hover:scale-[1.1] dark:hover:text-violet-300 hover:text-violet-800">
@@ -16,7 +18,7 @@ const Navbar = () => {
                     </a>
 
                 ))}
-            </div>
+            </div> */}
         </nav>
     );
 }
