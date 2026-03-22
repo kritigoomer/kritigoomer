843b3b7723569fc67ed43caf233f173c70a75422
Kriti Goomer <kritigoomer@gmail.com>
2026-03-22 01:29:48 -0500

    add chalo song

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className="flex w-screen justify-items-start md:justify-center">
             <nav className="fixed flex align-center justify-center items-center mx-4 hover:scale-[1.1] transition-transform">
             <a href="#">
-                <h1 className="font-heading text-3xl md:text-4xl hover:text-amber-300 hover:scale-[1.1] transition-colors">kg</h1>
+                <h1 className="font-heading text-3xl md:text-4xl hover:text-amber-300 hover:scale-[1.1] transition-all ease-in-out">kg</h1>
             </a>
             {/* <div className="font-body flex gap-6 text-xl">
                 {navLinks.map((link) => (
--- components/SongCard.jsx
+++ components/SongCard.jsx
@@ -1,23 +1,50 @@
+"use client";
+import { useState, useRef, useEffect } from "react";
+
+
 export default function SongCard() {
+    const song = {name: "chalo tumko lekar", 
+                singer: "shreya ghoshal",
+                songUrl: "/chalotumkolekar.mp3"}
+
+    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        audioRef.current = new Audio(song.songUrl);
+    }, []);
+
+    const togglePlayPause = () => {
+        if (!audioRef.current) return;
+
+        if (isPlaying) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    }
+
     return (
         <div className="absolute flex p-3 gap-3 top-1 md:top-28 right-6 rounded-lg 
                         opacity-85 outline-[1px] outline-white/30 
-                        dark:outline-white/10 text-shadow-black/50 text-shadow-xs
-                        backdrop-blur-2xl ease-in-out bg-white/10">
+                        text-shadow-black/50 text-shadow-xs
+                        backdrop-blur-2xl bg-white/10">
             <div className="aspect-square w-10">
                 <img src="/song.webp" className="w-full rounded-xs"/>
             </div>
             <div className="flex flex-col align-top">
-                <h1 className="font-body leading-none text-md">chalo tumko lekar</h1>
-                <h2 className="font-body text-xs">shreya ghoshal</h2>
+                <h1 className="font-body leading-none text-md">{song.name}</h1>
+                <h2 className="font-body text-xs">{song.singer}</h2>
                 <div className="mt-1 w-full bg-gray-300 rounded-full h-0.5">
                     <div id="progress-bar-fill"
                         className="w-0 bg-indigo-500 h-0.5 rounded-full transition-all duration-100 ease-linear"
                     ></div>
                 </div>
             </div>
-            <div className="flex h-fill items-center justify-center">
-                <img src="/play.svg" className="w-6" />
+            <div className="flex h-fill items-center justify-center"
+                    onClick={togglePlayPause}>
+                <img src={isPlaying ? "/pause.svg": "/play.svg"} className="w-6" />
             </div>
         </div>
     );
